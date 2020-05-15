57098c6e8f42acbc842740a468c258d98ef76aab
freddy91 <49192031+freddy91@users.noreply.github.com>
2020-05-15 10:36:39 +0200

    Add files via upload

---
+++ predic.php
@@ -0,0 +1,13 @@
+<?php
+   if(isset($_FILES['image'])){
+      
+    $nombre = $_FILES['image']['name'];
+     $tmp =$_FILES['image']['tmp_name'];
+      
+	 move_uploaded_file($tmp,"img/".$nombre);
+        
+   	$output = shell_exec("D:/python/python.exe predic.py $nombre");
+	echo $output;
+     
+   }
+?>
